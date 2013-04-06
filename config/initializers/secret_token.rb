8d52ce51e1f2183f550ed2143aedf75f75bc236f
sararob <sararob@brandeis.edu>
2013-04-06 09:09:43 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SaraRobinson::Application.config.secret_token = '4846adaf4ae788ea31de1e3c50133383b8d5976525a8e6606e960a5ba1daa26ec4559c9371b43f91a5111ebf5c3303dfb4286b0a6edcf89d1a281d93f94cc12c'
